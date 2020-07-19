f58b1f71d8702eb421487335fbe146fb68306776
marcon29 <online@markstabler.com>
2020-07-19 09:35:24 -0600

    Done.

---
+++ app/controllers/check_controller.rb
@@ -0,0 +1,5 @@
+# note: not part of class - added this to see how the controller class works in route calls
+
+class CheckController < ApplicationController
+   
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'about', to: 'static#about'
+  
+  # note: not part of class - added this to see how the controller class works in route calls
+  get 'check', to: 'check#check'
 end
