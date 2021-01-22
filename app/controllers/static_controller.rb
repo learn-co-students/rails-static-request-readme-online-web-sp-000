48288de752ae955996702433a243b18ebc147a61
rmholsen <jaguar.kitty@gmail.com>
2021-01-21 20:21:31 -0500

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,6 @@
+class StaticController < ApplicationController
+    def about
+        render "url_here"
+        #There should be a view here but I'm not going to bother until I get this ridiculous thing up and working. Or skip it I guess. 
+    end 
+end
